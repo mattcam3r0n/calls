7dc73ea63bcfc0861fa44097f6f9a3dcfde8a640
Matt Cameron <cameron.matt@gmail.com>
2021-07-09 20:44:13 -0500

    add axios fetchQuotes api

--- src/features/positions/positionsAPI.js
+++ src/features/positions/positionsAPI.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 const mockData = [
   {
     symbol: 'MSFT',
@@ -15,7 +17,30 @@ const mockData = [
 
 // A mock function to mimic making an async request for data
 export function fetchPositions() {
-  return new Promise((resolve) =>
-    setTimeout(() => resolve({ data: mockData }), 500)
-  );
+  console.log('fetchPositions');
+  const symbols = mockData.map(d => d.symbol);
+  console.log(symbols);
+  return fetchQuotes(symbols)
+    .then(resp => {
+      console.log(resp);
+      return new Promise((resolve) =>
+        setTimeout(() => resolve({ data: mockData }), 500)
+      );  
+    });
+}
+
+export function fetchQuotes(symbols) {
+  const url = "https://query2.finance.yahoo.com/v7/finance/quote?symbols=" + symbols.join(",");
+console.log(url);
+  return axios.get(url, {
+    headers: {
+      "Access-Control-Allow-Origin": "*"      
+    }
+  })
+    .then(resp => {
+      console.log(resp);
+    })
+    .catch(ex => {
+      console.log(ex);
+    });
 }
